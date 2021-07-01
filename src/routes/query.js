5505083dd8f865d76eb2892a2587037ed1409fdb
ginoscott <ginoscottdev@gmail.com>
2021-07-01 05:59:27 -1000

    new endpoint for querying namespace structure

--- src/routes/query.js
+++ src/routes/query.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const router = express.Router();
-const { dbFindQuery} = require('../database.js');
+const { dbInsert, dbFindOne, dbFindQuery} = require('../database.js');
 const { currentMJD } = require('../utils/time.js');
+const CosmosAgent = require('../utils/agent');
+const { ParseNamesToJSON } = require('../utils/data');
 
 //! following line is for parsing JSON data in POST requests
 router.use(bodyParser.json());
@@ -45,6 +47,46 @@ router.post('/soh/current/:nodeName/', (req, res) => {
         res.json(resp);
     });
 
+});
+
+router.post('/namespace/:node/:proc', (req, res) => {
+    const dbName = 'namespace';
+    const collection = `${req.params.node}`;
+    const options = req.body.options ? req.body.options : {};
+    const query = req.body.query ? req.body.query : {};
+
+    dbFindOne(dbName, collection, options, query, (stat, result) => {
+      if (stat.error) {
+        res.json({"error":"Error finding."});
+      } else {
+        if (result === null) {
+          // If namespace is not found, ask agent via request
+          console.log('here0');
+          const cmd = 'all_names_types';
+          const node = req.params.node;
+          const proc = req.params.proc;
+          CosmosAgent.AgentRequest(node, proc, cmd, 1000, (result) => {
+            if(typeof result == 'object') {
+              // Request error
+              return res.json(result);
+            } else {
+              try {
+                // Successful response
+                const output = ParseNamesToJSON(result);
+                dbInsert('namespace', node, output, () => {
+                  res.json(output);
+                });
+              } catch {
+                return res.json({ output: result });
+              }
+            }
+          });
+        } else {
+          // Query successful
+          res.json(result);
+        }
+      }
+    });
 });
 
 module.exports = router;
